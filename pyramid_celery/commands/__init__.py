eb793a78819ade168ab8d2d3bc53cb091e2cbd9d
Antti Haapala <antti.haapala@anttipatterns.com>
2014-09-11 13:50:17 +0300

    pyramid celery

--- pyramid_celery/commands/__init__.py
+++ pyramid_celery/commands/__init__.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 
 import sys
 
@@ -11,8 +11,9 @@ class CommandMixin(object):
 
     def setup_app_from_commandline(self, argv):
         if len(argv) < 2:
-            print >> sys.stderr, 'No configuration file specified.'
+            print('No configuration file specified.', file=sys.stderr)
             sys.exit(1)
+
         bootstrap(argv[1])
         self.app = default_app
         return argv[:1] + argv[2:]
